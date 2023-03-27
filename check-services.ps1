f1c1ee6ad1649638ed19c46ce31a3837da572d5f
Nick Marie <56579992+nickcode95@users.noreply.github.com>
2023-03-27 15:52:50 +1100

    Add files via upload

---
+++ check-services.ps1
@@ -0,0 +1,15 @@
+﻿$services = @("Base Filtering Engine", "DHCP Client", "Dnscache", "IKEEXT", "iphlpsvc", "nsi", "mpssvc", "RemoteRegistry")
+
+Foreach ($i in $services) {
+write $i
+}
+
+Set-Service -Name $services -Status "Running"
+
+Get-Service $services
+
+Set-Service -Name $services -Status "Automatic"
+
+Foreach ($i in $services) {
+Set-Service -Name $i -Status "Running"
+}
